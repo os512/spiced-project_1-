3e6677a5da01b3735ddbc207431486c322a933e3
Oliver Schupke <solanx@web.de>
2024-04-20 20:09:47 +0200

    Add toggle functionality for quiz card show answer button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const bookmarkIcons = document.querySelectorAll(".card__bookmark");
+const answer = document.querySelector(".card__answer");
+const buttonShowAnswer = document.querySelector(".quiz-button__showAnswer");
 
 /**
  * Toggles the state of bookmarks via click.
- * 
+ *
  * @param {NodeList} bookmarkIcons - The list of bookmark icons.
  */
 bookmarkIcons.forEach((bookmarkIcon) => {
@@ -15,3 +17,6 @@ bookmarkIcons.forEach((bookmarkIcon) => {
 	});
 });
 
+buttonShowAnswer.addEventListener("click", () => {
+	answer.classList.toggle("hidden");
+});
