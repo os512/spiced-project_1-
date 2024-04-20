9367c1e589fe860f3ecde775e7267ddb6861daa3
Oliver Schupke <solanx@web.de>
2024-04-20 19:04:07 +0200

    Refactor: Extend bookmark toggle functionality so that it can be applied to multiple quiz card bookmarks

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-const bookmarkIcon = document.querySelector(".card__bookmark");
+const bookmarkIcons = document.querySelectorAll(".card__bookmark");
 
 // Toggle state of bookmark
-// 
-bookmarkIcon.addEventListener("click", () => {
-	if (bookmarkIcon.getAttribute("src") === "./assets/bookmark.png") {
-		bookmarkIcon.setAttribute("src", "./assets/bookmark_filled.png");
-	} else {
-		bookmarkIcon.setAttribute("src", "./assets/bookmark.png");
-	}
+//
+bookmarkIcons.forEach((bookmarkIcon) => {
+	bookmarkIcon.addEventListener("click", () => {
+		if (bookmarkIcon.getAttribute("src") === "./assets/bookmark.png") {
+			bookmarkIcon.setAttribute("src", "./assets/bookmark_filled.png");
+		} else {
+			bookmarkIcon.setAttribute("src", "./assets/bookmark.png");
+		}
+	});
 });
-
