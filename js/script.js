c89aacf0292ba93bc87036a566029cd150239e8e
Oliver Schupke <solanx@web.de>
2024-04-21 19:39:27 +0200

    Refactor: Wrap js logic into IIFE to prevent scope issues

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
-const bookmarkIcons = document.querySelectorAll(".card__bookmark");
-const answers = document.querySelectorAll(".card__answer");
-const showAnswerButtons = document.querySelectorAll(".quiz-button__showAnswer");
+(function () {
+	"use strict";
 
-/**
- * Toggles the state of bookmarks via click.
- *
- * @param {NodeList} bookmarkIcons - The list of bookmark icons.
- */
-bookmarkIcons.forEach((bookmarkIcon) => {
-	bookmarkIcon.addEventListener("click", () => {
-		if (bookmarkIcon.getAttribute("src") === "./assets/bookmark.png") {
-			bookmarkIcon.setAttribute("src", "./assets/bookmark_filled.png");
-		} else {
-			bookmarkIcon.setAttribute("src", "./assets/bookmark.png");
-		}
+	const bookmarkIcons = document.querySelectorAll(".card__bookmark");
+	const answers = document.querySelectorAll(".card__answer");
+	const showAnswerButtons = document.querySelectorAll(".quiz-button__showAnswer");
+
+	/**
+	 * Toggles the state of bookmarks via click.
+	 *
+	 * @param {NodeList} bookmarkIcons - The list of bookmark icons.
+	 */
+	bookmarkIcons.forEach((bookmarkIcon) => {
+		bookmarkIcon.addEventListener("click", () => {
+			if (bookmarkIcon.getAttribute("src") === "./assets/bookmark.png") {
+				bookmarkIcon.setAttribute("src", "./assets/bookmark_filled.png");
+			} else {
+				bookmarkIcon.setAttribute("src", "./assets/bookmark.png");
+			}
+		});
 	});
-});
 
-/**
- * Shows/hides answers on click.
- *
- * @param {NodeList} showAnswerButtons - The list of show answer buttons.
- * @param {NodeList} answers - The list of answer elements.
- */
-showAnswerButtons.forEach((showAnswerButton, i) => {
-	showAnswerButton.addEventListener("click", () => {
-		if (showAnswerButton.textContent === "Hide answer") {
-			showAnswerButton.textContent = "Show answer";
-			showAnswerButton.setAttribute("aria-label", "Show answer");
-		} else {
-			showAnswerButton.textContent = "Hide answer";
-			showAnswerButton.setAttribute("aria-label", "Hide answer");
-		}
+	/**
+	 * Shows/hides answers on click.
+	 *
+	 * @param {NodeList} showAnswerButtons - The list of show answer buttons.
+	 * @param {NodeList} answers - The list of answer elements.
+	 */
+	showAnswerButtons.forEach((showAnswerButton, i) => {
+		showAnswerButton.addEventListener("click", () => {
+			if (showAnswerButton.textContent === "Hide answer") {
+				showAnswerButton.textContent = "Show answer";
+				showAnswerButton.setAttribute("aria-label", "Show answer");
+			} else {
+				showAnswerButton.textContent = "Hide answer";
+				showAnswerButton.setAttribute("aria-label", "Hide answer");
+			}
 
-		answers.forEach((answer, j) => (i === j ? answer.classList.toggle("hidden") : false));
+			answers.forEach((answer, j) => (i === j ? answer.classList.toggle("hidden") : false));
+		});
 	});
-});
+})();
